34f79ed95321d1f99719f4700c04ee26d8ba2eee
Shami Tomita <shamitomita@gmail.com>
2021-04-12 20:34:21 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,7 @@ class InteractiveRecord
   end 
   
   def self.find_by(column_name)
-    sql = "SELECT * FROM #{self.table_name} WHERE #{self.column_name} = ?"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{self.column_names} = ?"
     DB[:conn].execute(sql, column_name)
   end 
   
