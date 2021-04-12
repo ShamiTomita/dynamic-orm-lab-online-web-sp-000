15176b47a445801ac6f45ed6d8b4a99ada298fe3
Shami Tomita <shamitomita@gmail.com>
2021-04-12 20:39:44 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,14 +53,19 @@ class InteractiveRecord
     DB[:conn].execute(sql, name)
   end 
   
-  def self.find_by(hash)
-    key, value = hash.first
-    # if value.is_a? String
-    #   value = "'#{value}'"
-    # end
-    # binding.pry
-    sql = "SELECT * FROM #{self.table_name} WHERE #{key.to_s} = ?"
-    DB[:conn].execute(sql, value)
+ def self.find_by(attribute)
+    #executes the SQL to find a row by the attribute passed into the method
+    #WHERE name = ? OR grade = ? OR id = ?
+    #attribute is a hash, so it has a key/value pair
+    attribute_key = attribute.keys.join()
+    attrubute_value = attribute.values.first
+    sql =<<-SQL
+      SELECT * FROM #{self.table_name}
+      WHERE #{attribute_key} = "#{attrubute_value}"
+      LIMIT 1
+    SQL
+    row = DB[:conn].execute(sql)
   end
+  
   
 end
